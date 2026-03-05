4bb638c2ebb9b7df3edce5ffe22f98a9a0ea1006
aicu-icu <ai.aicu.icu@gmail.com>
2026-03-05 14:00:10 +0800

    Add extension icons (16/32/48/128px)

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -8,5 +8,11 @@ export default defineConfig({
     description: '将任意网页转换为 API 和 MCP 服务',
     permissions: ['storage'],
     host_permissions: ['<all_urls>'],
+    icons: {
+      16: '/icon/16.png',
+      32: '/icon/32.png',
+      48: '/icon/48.png',
+      128: '/icon/128.png',
+    },
   },
 });
